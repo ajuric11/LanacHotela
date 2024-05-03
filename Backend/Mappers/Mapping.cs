cf8a150e3630f61f669bdc349134ae9de8666e33
ajuric11 <156127822+ajuric11@users.noreply.github.com>
2024-05-03 11:03:06 +0200

    Add files via upload

---
+++ Backend/Mappers/Mapping.cs
@@ -0,0 +1,53 @@
+﻿
+using AutoMapper;
+
+namespace Backend.Mappers
+{
+    public class Mapping<T, DTR, DTI>
+    {
+
+        protected Mapper MapperMapReadToDTO;
+        protected Mapper MapperMapInsertUpdatedFromDTO;
+        protected Mapper MapperMapInsertUpdateToDTO;
+
+        public Mapping()
+        {
+            MapperMapReadToDTO = new Mapper(new MapperConfiguration(c => {
+                c.AllowNullDestinationValues = true;
+                c.CreateMap<T, DTR>();
+            }));
+            MapperMapInsertUpdatedFromDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<DTI, T>();
+            }));
+
+            MapperMapInsertUpdateToDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<T, DTI>();
+            }));
+        }
+
+        public List<DTR> MapReadList(List<T> lista)
+        {
+            var vrati = new List<DTR>();
+            lista.ForEach(e => { vrati.Add(MapReadToDTO(e)); });
+            return vrati;
+        }
+
+        public DTR MapReadToDTO(T entitet)
+        {
+            return MapperMapReadToDTO.Map<DTR>(entitet);
+        }
+
+        public T MapInsertUpdatedFromDTO(DTI entitet)
+        {
+            return MapperMapInsertUpdatedFromDTO.Map<T>(entitet);
+        }
+
+        public DTI MapInsertUpdateToDTO(T entitet)
+        {
+            return MapperMapInsertUpdateToDTO.Map<DTI>(entitet);
+        }
+
+
+
+    }
+}
---
+++ Backend/Mappers/MappingSoba.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Backend.Models;
+
+namespace Backend.Mappers
+{
+    public class MappingSoba : Mapping<Soba, SobaDTORead, SobaDTOInsertUpdate>
+    {
+
+        public MappingSoba()
+        {
+            MapperMapReadToDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<Soba, SobaDTORead>()
+                .ConstructUsing(entitet =>
+                 new SobaDTORead(
+
+                     entitet.sifra,
+                    entitet.Oznaka,
+                    entitet.Kapacitet
+                    ));
+            }));
+
+            MapperMapInsertUpdatedFromDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<SobaDTOInsertUpdate, Soba>();
+            }));
+
+            MapperMapInsertUpdateToDTO = new Mapper(new MapperConfiguration(c => {
+                c.CreateMap<Soba, SobaDTOInsertUpdate>()
+                .ConstructUsing(entitet =>
+                 new SobaDTOInsertUpdate(
+                   entitet.Oznaka,
+                   entitet.Kapacitet
+                  ));
+            }));
+        }
+
+    }
+}
